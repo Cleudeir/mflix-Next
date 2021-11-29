753746bb40d47abd320f4f6b49b14f4291c2f061
Cleudeir <cleudeirsilva@gmail.com>
2021-11-29 01:24:13 -0300

    Update Last_view.jsx

--- components/Last_view.jsx
+++ components/Last_view.jsx
@@ -1,17 +1,17 @@
-import style from "../styles/last_view.module.css";
+import styles from "../styles/last_view.module.css";
 function Last_view({ filmes }) {
   return (
     <div>
       <section
-        className={style.background}
+        className={styles.background}
         style={{
           backgroundImage: `url(${
             filmes[Math.floor(Math.random() * filmes.length)].backdrop_path
           })`,
         }}
       >
-        <div className={style.Vertical}>
-          <div className={style.horizontal}></div>
+        <div className={styles.Vertical}>
+          <div className={styles.horizontal}></div>
         </div>
       </section>
     </div>
