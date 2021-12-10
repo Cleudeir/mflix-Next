c4b25ef3ef824f685133b764653ad3f103ef10a7
Cleudeir <82124121+Cleudeir@users.noreply.github.com>
2021-12-09 23:41:13 -0300

    update

--- pages/api/crawling.js
+++ pages/api/crawling.js
@@ -7,10 +7,10 @@ function Crawling(req, res) {
       const browser = await puppeteer.launch(
         process.env.NODE_ENV === "production"
           ? {
-            args: chrome.args,
-            executablePath: await chrome.executablePath,
-            headless: chrome.headless,
-          }
+              args: chrome.args,
+              executablePath: await chrome.executablePath,
+              headless: chrome.headless,
+            }
           : {}
       );
       const page = await browser.newPage();
@@ -21,31 +21,28 @@ function Crawling(req, res) {
       });
       const mount = "<table>" + table + "</table>";
 
-      //html to json
       const json = await HtmlTableToJson.parse(mount);
       const result = await json._results[0].reverse();
-
-      //--
       const array_id_sort = [];
       const result_ids = [];
       if (props.type === "movie") {
         result.map((x) => {
           array_id_sort.push({
             id: x.IMDB,
-            date: +x["Data de publicação"].slice(0, 4),
+            date: +x['Data de publicação'].slice(0, 4),
           });
         });
         const result_filter = array_id_sort
           .filter((x) => x.date >= 2021)
           .filter((x) => x !== false);
         result_filter.map((x) => result_ids.push(x.id));
-        fs.writeFileSync(`./data/ids_movie.json`, JSON.stringify(result_ids));
+        fs.writeFileSync("./data/ids_movie.json", JSON.stringify(result_ids));
       }
       //--
       if (props.type === "tv") {
         result.map((x) => {
           array_id_sort.push({
-            id: x["ID - THEMOVIEDB"],
+            id: x['ID - THEMOVIEDB'],
             date: +x["Última atualização"].slice(0, 4),
           });
         });
@@ -53,7 +50,7 @@ function Crawling(req, res) {
           .filter((x) => x.date >= 2019)
           .filter((x) => x !== false);
         result_filter.map((x) => result_ids.push(x.id));
-        fs.writeFileSync(`./data/ids_tv.json`, JSON.stringify(result_ids));
+        fs.writeFileSync("./data/ids_tv.json", JSON.stringify(result_ids));
       }
       await browser.close();
       return result_ids;
